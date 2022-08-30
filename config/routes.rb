de37a35f6566c4913200ef183158db5cc3d23f36
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-30 15:10:13 -0300

    public recipes page

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
 
-  root "users#index"
+  root 'recipes#public_recipes'
 
   resources :users do
     resources :recipe_foods, only: [:index, :new, :create, :destroy]
     resources :foods, only: [:index, :new, :create, :destroy]
     resources :recipes
   end
+
+  get 'public_recipes' => 'recipes#public_recipes'
 end
