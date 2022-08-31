1d66e1722f60a51cb1c8614969695393844b60cc
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-08-31 14:47:05 +0200

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
+  
+
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
 
-  root "users#index"
-
+  root "recipes#index"
+  
   resources :users do
-    resources :recipe_foods, only: [:index, :new, :create, :destroy]
+    resources :recipes do
+      resources :recipe_foods
+    end
     resources :foods
-    resources :recipes
   end
+
+  # resources :foods
+  # resources :recipes do
+  #   resources :recipe_foods
+  # end
 end
