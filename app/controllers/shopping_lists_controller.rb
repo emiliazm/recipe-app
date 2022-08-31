d03e3c179ec6d8db5bb94c9a8408fa9c5aa7325e
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-31 17:32:11 -0300

    delete comments

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,8 +1,5 @@
 class ShoppingListsController < ApplicationController
   def index
-    # @recipe_foods = RecipeFood.all
-    # @all_foods = current_user.foods.where(id: @recipe_foods).order(:id)
-    # @food_number = @all_foods.length
     @foods = current_user.foods
   end
 end
