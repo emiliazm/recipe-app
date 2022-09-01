bf8e56da1fbe3cd6b2cb5005a0374203a9222171
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-09-01 15:23:38 +0200

    Fix public recipes totals

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,7 +1,6 @@
 class PublicRecipesController < ApplicationController
+  include PublicRecipesHelper
   def index
     @recipes = Recipe.where(public: true).order(created_at: :desc)
-    # @foods = Food.where(id: @recipe_foods)
-    @foods = current_user.foods.where(id: @recipe_foods).order(:id)
   end
 end
